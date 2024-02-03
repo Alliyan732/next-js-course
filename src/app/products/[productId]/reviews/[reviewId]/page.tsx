73187bd6f86eb10421b1b265672467b3d4c08f10
Alliyan <alliyan732@gmail.com>
2024-02-03 19:33:57 +0500

    page not found customized

--- src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,3 +1,12 @@
-export default function ReviewDetail({params} : {params : {productId : string, reviewId: string}}) {
-    return <h1>Review {params.reviewId} for Product {params.productId}</h1>
+import { notFound } from "next/navigation"
+
+export default function ReviewDetail({ params } : {params : {productId : string, reviewId: string}}) {
+    
+    if (parseInt(params.reviewId) > 100) {
+        notFound();
+    }
+    else {
+        return <h1>Review {params.reviewId} for Product {params.productId}</h1>
+
+    }
 }
